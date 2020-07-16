1e5938a22c291343898c4c213f9415024cfa29bd
abdelrahman-M-abdelfatah <abdoabdo11223399@gmail.com>
2020-07-16 17:41:49 +0200

    Initial Commit

---
+++ auto-layout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  auto-layout
+//
+//  Created by admin on 7/16/20.
+//  Copyright © 2020 admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
